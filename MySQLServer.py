1e99d1c7e877f1c71b390856c90cce334b55602a
akramiga <akramlubega73@gmail.com>
2025-02-02 16:21:46 +0300

    pyhton script creating a databse

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+host = "localhost"
+user = "root"
+password = "diego3107"
+database = "alx_book_store"
+try:
+    connection = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+        print(f"Database '{database}' created successfully or already exists.")
+
+except Error as e:
+    print(f"Error: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed.")    
